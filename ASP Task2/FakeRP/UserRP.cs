ad4ba9d35f9ff0e44580c04f8dffb49d9b49b418
Elxan Atayev <elxanatayev.la@gmail.com>
2022-04-04 12:13:09 +0400

    Add project files.

---
+++ ASP Task2/FakeRP/UserRP.cs
@@ -0,0 +1,41 @@
+﻿using ASP_Task2.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_Task2.FakeRP
+{
+    public class UserRP
+    {
+        public static List<User> Users = new List<User>
+        {
+            new User
+            {
+                Id=1,
+                Name="Cristiano",
+                Surname="Ronaldo",
+                Age=38
+            },
+            new User
+            {
+                Id=1,
+                Name="Leonel",
+                Surname="Messi",
+                Age=34
+            },
+            new User
+            {
+                Id=1,
+                Name="Alex",
+                Surname="Sander",
+                Age=22
+            }
+        };
+
+        public List<User> GetAllUsers()
+        {
+            return Users;
+        }
+    }
+}
